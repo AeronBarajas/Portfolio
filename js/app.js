a6912b25be97d74074e4782d74a7f79135a40cea
Aeron Barajas <aeronbarajas@Aerons-MacBook-Air.local>
2021-07-14 16:40:51 -0500

    m

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
-// var acc = document.getElementsByClassName("accordion");
-// var i;
+const acc = document.getElementsByClassName("accordion");
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+const $menu = $('.menu')
+const $navlink = $('.navlink')
+
