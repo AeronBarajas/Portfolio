aa43fa0bc2be7d928a65a04c86d66ac85108f125
Aeron Barajas <aeronbarajas@Aerons-MacBook-Air.local>
2021-07-14 23:29:17 -0500

    m

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
-const acc = $(".accordion");
+// const acc = $(".accordion");
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    const panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     const panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
 
 
-const menu = $('.menu')
-const navlink = $('ul')
+// const menu = $('.menu')
+// const navlink = $('ul')
 // let show = false;
 
 
 
-for (i = 0; i < menu.length; i++) {
+// for (i = 0; i < menu.length; i++) {
     
-    menu[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      const ul = this.nextElementSibling;
-      if (ul.style.display === "block") {
-        ul.style.display = "none";
-      } else {
-        ul.style.display = "block";
-      }
-    });
-  }
+//     menu[i].addEventListener("click", function() {
+//       this.classList.toggle("active");
+//       const ul = this.nextElementSibling;
+//       if (ul.style.display === "block") {
+//         ul.style.display = "none";
+//       } else {
+//         ul.style.display = "block";
+//       }
+//     });
+//   }
   
 
 
