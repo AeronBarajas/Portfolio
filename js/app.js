08bd2b20a91521e8c2fb124a2d0e73719c5a817b
Aeron Barajas <aeronbarajas@Aerons-MacBook-Air.local>
2021-07-14 22:21:59 -0500

    m

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const acc = document.getElementsByClassName("accordion");
+const acc = $(".accordion");
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
@@ -12,6 +12,40 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-const $menu = $('.menu')
-const $navlink = $('.navlink')
 
+const menu = $('.menu')
+const navlink = $('ul')
+// let show = false;
+
+
+
+for (i = 0; i < menu.length; i++) {
+    
+    menu[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      const ul = this.nextElementSibling;
+      if (ul.style.display === "block") {
+        ul.style.display = "none";
+      } else {
+        ul.style.display = "block";
+      }
+    });
+  }
+  
+
+
+// const showMenu = (event) => {
+//     if (show){
+//         $navlink.each(function(i){
+//             $(this).css('display', 'none')
+//         })
+//         show = false
+//     } else {
+//         $navlink.each(function(i){
+//             $(this).css('display', 'block')
+//         })
+//         show = true
+//     }
+// }
+
+// $menu.on('click', showMenu)
