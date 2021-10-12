62bd15e500afe837d9196b821c2ef4a8d8dfe1de
Aeron Barajas <aeronbarajas@Aerons-MacBook-Air.local>
2021-10-12 16:46:39 -0500

    set links to target blank

--- js/app.js
+++ js/app.js
@@ -5,12 +5,12 @@ $.ajax('./json/projects.json')
     data.forEach((project) => {
         const div = $("<div class ='project'>")
         div.html(`
-        <a href='${project.deployed}'>
+        <a href='${project.deployed}' target='_blank'>
         <img src= '${project.image}'>
         <h4>${project.name}</h4>
         </a>
         <p>${project.description}</p>
-        <a href='${project.github}'>
+        <a href='${project.github}' target='_blank'>
         <p class='repo-link'>Repo</p>
         </a>`)
         projects.append(div)
